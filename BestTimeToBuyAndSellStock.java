24b613ee299cbef209fc6d391935491588d94f22
KhageshUmbarkar <khageshumbarkar9@gmail.com>
2025-07-25 00:04:44 +0530

    Add more optimal solution
    updated the code and reduced the time complexity to O(n)

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -1,15 +1,26 @@
-// The below code gives executes for all test cases, but has O(n^2) time complexity
 class BestTimeToBuyAndSellStock{
 	public static void main(String[] args) {
-		int prices [] = {7,6,4,3,1};
+		int prices [] = {7,1,5,3,6,4};
 		System.out.println(maxProfit(prices));
 	}
 	public static int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        for(int i=0;i<prices.length;i++)
-            for(int j=i+1;j<prices.length;j++)
-                if((prices[j]-prices[i])>maxProfit)
-                    maxProfit = prices[j] - prices[i];
-        return maxProfit;
+        int min = prices[0];
+        int profit = 0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min)
+            	min = prices[i];
+            profit = Math.max(profit, prices[i]-min);
+        }
+        return profit;
     }
+
+// The below code gives executes for all test cases, but has O(n^2) time complexity
+	// public static int maxProfit(int[] prices) {
+    //     int maxProfit = 0;
+    //     for(int i=0;i<prices.length;i++)
+    //         for(int j=i+1;j<prices.length;j++)
+    //             if((prices[j]-prices[i])>maxProfit)
+    //                 maxProfit = prices[j] - prices[i];
+    //     return maxProfit;
+    // }
 }
